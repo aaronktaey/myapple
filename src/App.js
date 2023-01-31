f9bdf3d1f549d9a6cf81754cd1d6a1caa8287945
aaronktaey <aaronktaey@gmail.com>
2023-01-31 17:28:37 +0900

    디펜던시 다시 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+styled.button`
+  width : 30px;
+  height : 50px;
+`
 
 let post = "강남우동맛집";
 
@@ -11,28 +17,31 @@ function App() {
         <h4>
           여기는 헤더
         </h4>
-      </div>
-      <div className="Navbar">
-        <nav>
-        여기는 네브바
-        <ul>
-          <li>About</li>
-          <li>Shop</li>
-          <li>Contact</li>
-        </ul>
-        </nav>
+        <div className="Navbar">
+          <nav>
+          여기는 네브바
+          <ul>
+            <li>About</li>
+            <li>Shop</li>
+            <li>Contact</li>
+          </ul>
+          </nav>
+        </div>
       </div>
       <div className="Contents">
         <h4>{글제목[0]}</h4>
         <p>진짜 이쁜 코트</p>
+        <button>자세히</button>
       </div>
       <div className="Contents">
         <h4>{글제목[1]}</h4>
         <p>삼겹살 맛집 등장</p>
+        <button>자세히</button>
       </div>
       <div className="Contents">
         <h4>{글제목[2]}</h4>
         <p>useState가 뭐여</p>
+        <button>자세히</button>
       </div>
       <div className="Footer">
         여기는 푸터
