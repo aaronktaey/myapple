2b475a0ac6c76ffe15aef10ebbceff50a8a34c70
aaronktaey <aaronktaey@gmail.com>
2023-02-01 14:29:10 +0900

    어바웃 패딩탑 다시 수정

--- src/About.js
+++ src/About.js
@@ -4,10 +4,7 @@ function About() {
   return (
     <div id="about" className="about-container">
       <div className="about-item">
-        여기는 사이트
-      </div>
-      <div className="about-item">
-        소개가 들어갈 자리
+        여기는 사이트 소개가 들어갈 자리
       </div>
     </div>
   );
