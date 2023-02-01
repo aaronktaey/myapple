f1c3f6f118b7d41c50f891bd413f23d3f43b836f
aaronktaey <aaronktaey@gmail.com>
2023-02-01 10:05:17 +0900

    App.test 제거, App.js 정리

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-import { useState } from "react";
-import styled from "styled-components";
 import Nav from "./Nav";
 
-const StyledDiv = styled.div`
-  background-color: grey;
-  color: white;
-`;
 
-let post = "강남우동맛집";
 
 function App() {
-  let [글제목, b] = useState(["여자코드추천", "강남맛집추천", "리액트독학"]);
 
   return (
     <div className="App">
       <Nav/>
-      <div className="Contents">
-        <h4>{글제목[0]}</h4>
-        <p>진짜 이쁜 코트</p>
-        <button>자세히</button>
-      </div>
-      <div className="Contents">
-        <h4>{글제목[1]}</h4>
-        <p>삼겹살 맛집 등장</p>
-        <button>자세히</button>
-      </div>
-      <div className="Contents">
-        <h4>{글제목[2]}</h4>
-        <p>useState가 뭐여</p>
-        <button>자세히</button>
-      </div>
-      <div className="Footer">여기는 푸터</div>
+      {/* About */}
+      {/* Contents */}
+      {/* Contacts */}
     </div>
   );
 }
