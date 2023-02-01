57f9ea49beab8a4234c8ef008a1014c383f3ff4c
aaronktaey <aaronktaey@gmail.com>
2023-02-01 11:01:47 +0900

    어바웃 추가 및 플렉스 적용

--- src/About.js
+++ src/About.js
@@ -2,8 +2,22 @@ import "./About.css"
 
 function About() {
   return (
-    <div>
-      
+    <div id="about" className="flex-container">
+      <div className="flex-item">
+        여기는
+      </div>
+      <div className="flex-item">
+        사이트
+      </div>
+      <div className="flex-item">
+        소개가
+      </div>
+      <div className="flex-item">
+        들어갈
+      </div>
+      <div className="flex-item">
+        자리
+      </div>
     </div>
   );
 }
