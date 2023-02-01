f1d20385e70afdcc9722a7e3e3b5fb9491f799f5
aaronktaey <aaronktaey@gmail.com>
2023-02-01 11:28:53 +0900

    컨텐츠 컴포넌트 추가 / 플렉스 적용

---
+++ src/Contents.js
@@ -0,0 +1,25 @@
+import "./Contents.css"
+
+function Contents() {
+  return (
+    <div id="contents" className="contents-container">
+      <div className="contents-item">
+        상품1
+      </div>
+      <div className="contents-item">
+        상품2
+      </div>
+      <div className="contents-item">
+        상품3
+      </div>
+      <div className="contents-item">
+        상품4
+      </div>
+      <div className="contents-item">
+        상품5
+      </div>
+    </div>
+  );
+}
+
+export default Contents;
