5a44f1db3fab0a2ed109d1cebc58e054bb7db28d
aaronktaey <aaronktaey@gmail.com>
2023-02-07 11:40:43 +0900

    리액트 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import Nav from "./Nav";
 import About from "./About";
 import Contents from "./Contents";
 import Footer from "./Footer";
-
+import NotFound from "./NotFound";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
       <Nav/>
-      <About/>
-      <Contents/>
+      <Routes>
+        <Route path="/About" element={<About/>}></Route>
+        <Route path="/Contents" element={<Contents/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
       <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -6,8 +6,8 @@ function Nav() {
       <nav className="Navs">
           <span><a href="">My Apple</a></span>
         <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contents">Contents</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contents">Contents</a></li>
           <li><a href="#footer">Contacts</a></li>
         </ul>
       </nav>
