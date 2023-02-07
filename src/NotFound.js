faf68b95e3a6e560f484621e11d1916ff1dc640a
aaronktaey <aaronktaey@gmail.com>
2023-02-07 11:41:11 +0900

    URL 낫파운드 추가

---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+import "./NotFound.css"
+
+function NotFound() {
+  return (
+    <div id="notfound" className="notfound-container">
+      <div className="notfound-item">
+        <h1>
+          Page NotFound.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
